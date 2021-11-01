8817633d9f04d643cb81c501bce9fa03ed8c147c
Sivakar Sithamparanathan <sivakar12@outlook.com>
2021-11-01 19:51:30 +0530

    Add a sample initial data

---
+++ src/initial-data.ts
@@ -0,0 +1,47 @@
+import { Habit } from "./types";
+
+const habits: Habit[] = [
+    {
+        id: '1',
+        name: 'Drink Water',
+        createdTime: new Date(),
+        archived: false,
+        logs: []
+    },
+    {
+        id: '2',
+        name: 'Do Planks',
+        createdTime: new Date(),
+        archived: false,
+        logs: [
+            {
+                time: new Date(2020, 10, 5, 12, 11, 23)
+            },
+            {
+                time: new Date(2020, 10, 6, 7, 10, 43)
+            }
+        ]
+    },
+    {
+        id: '3',
+        name: 'Meditate',
+        createdTime: new Date(),
+        archived: false,
+        logs: [
+            {
+                time: new Date(2020, 11, 2, 12, 11, 23)
+            },
+            {
+                time: new Date(2020, 12, 2, 12, 11, 23)
+            },
+            {
+                time: new Date(2020, 12, 3, 12, 34, 3)
+            },
+            {
+                time: new Date(2020, 12, 4, 7, 12, 3)
+            }
+        ]
+    }
+]
+
+export { habits as default } 
