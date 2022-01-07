6460c6c4dd83f7b25b5339e431b57084531bfc2e
Sivakar Sithamparanathan <sivakar12@outlook.com>
2022-01-07 22:25:12 +0530

    Change sample data

--- app/SampleData.ts
+++ app/SampleData.ts
@@ -27,21 +27,56 @@ const habits: Habit[] = [
         name: 'Drink Water',
         createdTime: (new Date()).toISOString(),
         archived: false,
-        logs: createRandomDates(sampleStartDate, sampleEndDate, 1000).map(dateToHabitLog)
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 1834).map(dateToHabitLog)
     },
     {
         id: '1',
         name: 'Do Planks',
         createdTime: (new Date()).toISOString(),
         archived: false,
-        logs: createRandomDates(sampleStartDate, sampleEndDate, 500).map(dateToHabitLog)
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 235).map(dateToHabitLog)
     },
     {
         id: '2',
         name: 'Meditate',
         createdTime: (new Date()).toISOString(),
         archived: false,
-        logs: createRandomDates(sampleStartDate, sampleEndDate, 750).map(dateToHabitLog)
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 788).map(dateToHabitLog)
+    },
+    {
+        id: '3',
+        name: 'Resist Social Media',
+        createdTime: (new Date()).toISOString(),
+        archived: false,
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 3453).map(dateToHabitLog)
+    },
+    {
+        id: '4',
+        name: 'Journal',
+        createdTime: (new Date()).toISOString(),
+        archived: false,
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 567).map(dateToHabitLog)
+    },
+    {
+        id: '5',
+        name: 'Control anger',
+        createdTime: (new Date()).toISOString(),
+        archived: false,
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 876).map(dateToHabitLog)
+    },
+    {
+        id: '6',
+        name: 'Eat/drink something healthy',
+        createdTime: (new Date()).toISOString(),
+        archived: false,
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 78).map(dateToHabitLog)
+    },
+    {
+        id: '7',
+        name: 'Hold a good posture',
+        createdTime: (new Date()).toISOString(),
+        archived: false,
+        logs: createRandomDates(sampleStartDate, sampleEndDate, 4).map(dateToHabitLog)
     }
 ]
 
